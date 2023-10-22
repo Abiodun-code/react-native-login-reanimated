10917c18916f54978343fd8991b449082ba9ef87
Abiodun-code <olaabiodun157@gmail.com>
2023-10-22 10:25:41 -0700

    login styling done

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/screens/LoginScreen';
+import LoginScreen from './screens/LoginScreen';
 import { RootStack } from './src/types/stackParam';
 
 const Stack = createNativeStackNavigator<RootStack>();
