72eec3f6b71ddfdb433ec9894a06bd046fc02b18
Abiodun-code <olaabiodun157@gmail.com>
2023-10-23 01:42:51 -0700

    sign up styling done

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import { RootStack } from './src/types/stackParam';
+import SignScreen from './screens/SignScreen';
 
 const Stack = createNativeStackNavigator<RootStack>();
 
@@ -12,6 +13,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Sign" component={SignScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
